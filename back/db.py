eddb1245c7e450654f898142a18b25147c5ffeed
xbgm08 <gabrielbraulino@gmail.com>
2025-06-24 10:24:04 -0300

    feat(backend): adiciona configuração de ambiente e atualiza conexão com o banco de dados para usar dotenv

--- back/db.py
+++ back/db.py
@@ -1,10 +1,14 @@
 import psycopg2
+from dotenv import load_dotenv
+import os
+
+load_dotenv() 
 
 def get_conn():
     return psycopg2.connect(
-        user="postgres",
-        host="localhost",
-        database="LEGT",
-        password="03062229",
-        port=5433
+        user=os.getenv("DB_USER"),
+        host=os.getenv("DB_HOST"),
+        database=os.getenv("DB_NAME"),
+        password=os.getenv("DB_PASSWORD"),
+        port=os.getenv("DB_PORT")
     )
